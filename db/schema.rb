11cfa2b30e15b1f6e38ff201a58cb8c0f08e5c10
FuwariSprit <furufuwa1021@gmail.com>
2018-12-06 09:12:59 +0900

    update_db内の一意性を保つ

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205224502) do
+ActiveRecord::Schema.define(version: 20181206000220) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
